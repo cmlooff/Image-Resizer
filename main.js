661e8405ee569ddc437bd8c43cea8e741b681d75
cmloof <cmlooff@gmail.com>
2022-09-28 13:45:07 -0400

    Add platform & environment checks. Add macOS window app standard functionality as well

--- main.js
+++ main.js
@@ -7,18 +7,36 @@ const { app, BrowserWindow } = require('electron');
 // Create a mainWindow variable so we can access it within the global stack
 let mainWindow;
 
+/** Environment Query
+ * Are we in production, or development environment?
+ * Are we in windows, max, or linux?
+ *
+ * Currently we strictly set this to development for development.
+ *
+ * @isDev gives us a bool value to check whether process.env.NODE_ENV is in development mode or not
+ *
+ * @ProcessPlatform process.platform -> Gives us the OS the user is on. I'm on windows therefore win32
+ */
+process.env.NODE_ENV = 'development';
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isWin = process.platform === 'win32' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
 // Creating a function to create our desktop window
 function createMainWindow() {
   /** Instantiating a new browser window
    * Browser window can take in an object of [options]
    * https://www.electronjs.org/docs/latest/api/browser-window
    * ? There are a ton of options/properties for the BrowserWindow -> Imagine CSS and HTML all in one
+   *
+   * Resizable: If we're in development we want to be able to access our devtools by expanding the window. We don't want production to do that
    */
   mainWindow = new BrowserWindow({
     title: 'ImageOptimizer',
     width: 500,
     height: 600,
-    icon: `${__dirname}/assets/icons/Icon_256x256.png`
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev
   });
 
   /** loadUrl / loadFile
@@ -45,3 +63,21 @@ function createMainWindow() {
  *
  */
 app.on('ready', createMainWindow);
+
+//// MacOS
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  /** Notes
+   * Common for macOS to keep applications and their menus
+   * to stay active until the user quits explicitly
+   */
+  if (!isMac) app.quit();
+});
+
+app.on('activate', () => {
+  /** Notes
+   * Common for macOS to re-create a window in the app when the
+   * dock icon is clicked and there are no other windows open
+   */
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
